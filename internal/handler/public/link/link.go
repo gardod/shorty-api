6595a60294ba4c5ac7c08be543dd410694ac6b90
Andrej Tratnik <4686037+gardod@users.noreply.github.com>
2019-07-17 22:32:54 +0200

    routes for link

---
+++ internal/handler/public/link/link.go
@@ -0,0 +1,15 @@
+package link
+
+import "net/http"
+
+func list(w http.ResponseWriter, r *http.Request) {}
+
+func create(w http.ResponseWriter, r *http.Request) {}
+
+func getByShort(w http.ResponseWriter, r *http.Request) {}
+
+func get(w http.ResponseWriter, r *http.Request) {}
+
+func update(w http.ResponseWriter, r *http.Request) {}
+
+func delete(w http.ResponseWriter, r *http.Request) {}
--- internal/handler/public/router.go
+++ internal/handler/public/router.go
@@ -3,11 +3,14 @@ package public
 import (
 	"net/http"
 
+	"github.com/gardod/shorty-api/internal/handler/public/link"
 	"github.com/go-chi/chi"
 )
 
 func GetRouter() http.Handler {
 	r := chi.NewRouter()
+
+	r.Mount("link", link.GetRouter())
 
 	return r
 }
