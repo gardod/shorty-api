2d91f6a1b6259fcd82a070d666e953e29834e3cb
Andrej Tratnik <4686037+gardod@users.noreply.github.com>
2020-01-12 10:35:33 +0100

    add some default middlewares

--- internal/handler/public/router.go
+++ internal/handler/public/router.go
@@ -5,10 +5,19 @@ import (
 
 	"github.com/gardod/shorty-api/internal/handler/public/link"
 	"github.com/go-chi/chi"
+	"github.com/go-chi/chi/middleware"
 )
 
 func GetRouter() http.Handler {
 	r := chi.NewRouter()
+
+	// TODO: implement own requestID and logger middleware with logrus
+	r.Use(
+		middleware.DefaultCompress,
+		middleware.StripSlashes,
+		middleware.RequestID,
+		middleware.Logger,
+	)
 
 	r.Mount("/link", link.GetRouter())
 
