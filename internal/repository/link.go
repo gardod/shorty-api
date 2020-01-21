f8255fb86083f8afcaba10ce32d6b851fa2ccd47
Andrej Tratnik <4686037+gardod@users.noreply.github.com>
2020-01-21 23:11:15 +0100

    link repo

---
+++ internal/repository/link.go
@@ -0,0 +1,250 @@
+package repository
+
+import (
+	"context"
+	"database/sql"
+	"fmt"
+
+	"github.com/gardod/shorty-api/internal/driver/postgres"
+	"github.com/gardod/shorty-api/internal/middleware"
+	"github.com/gardod/shorty-api/internal/model"
+	"github.com/sirupsen/logrus"
+)
+
+type Link struct {
+	log     logrus.FieldLogger
+	db      *postgres.Client
+	q       QueryOptions
+	withDel bool
+}
+
+func NewLink(ctx context.Context) *Link {
+	return &Link{
+		log: middleware.GetLogger(ctx),
+		db:  middleware.GetDB(ctx),
+		q:   QueryOptions{},
+	}
+}
+
+func (r *Link) Insert(ctx context.Context, link *model.Link) error {
+	query := `
+		INSERT INTO "link" (
+			"short",
+			"long"
+		) VALUES (
+			$1,
+			$2
+		)
+		RETURNING
+			"created_at",
+			"updated_at"
+	`
+
+	row := r.db.QueryRowContext(ctx, query, link.Short, link.Long)
+
+	err := row.Scan(&link.CreatedAt, &link.UpdatedAt)
+	if err != nil {
+		r.log.WithError(err).Error("Unable to insert Link")
+		return err
+	}
+
+	return nil
+}
+
+func (r *Link) Update(ctx context.Context, link *model.Link) error {
+	query := `
+		UPDATE "link"
+		SET "short" = $2,
+			"long" = $3,
+			"updated_at" = now()
+		WHERE "id" = $1
+		RETURNING "updated_at"
+	`
+
+	row := r.db.QueryRowContext(ctx, query, link.ID, link.Short, link.Long)
+
+	err := row.Scan(&link.UpdatedAt)
+	if err != nil {
+		if err != sql.ErrNoRows {
+			r.log.WithError(err).Error("Unable to update Link")
+		}
+		return err
+	}
+
+	return nil
+}
+
+func (r *Link) Delete(ctx context.Context, link *model.Link) error {
+	query := `
+		UPDATE "link"
+		SET "deleted_at" = now()
+		WHERE "id" = $1
+		RETURNING "deleted_at"
+	`
+
+	row := r.db.QueryRowContext(ctx, query, link.ID)
+
+	err := row.Scan(&link.DeletedAt)
+	if err != nil {
+		if err != sql.ErrNoRows {
+			r.log.WithError(err).Error("Unable to delete Link")
+		}
+		return err
+	}
+
+	return nil
+}
+
+func (r *Link) Get(ctx context.Context) ([]model.Link, error) {
+	q := r.q
+	if !r.withDel {
+		q.Where = append(q.Where, `"l"."deleted_at" IS NULL`)
+	}
+
+	query := fmt.Sprintf(`
+		SELECT
+			%s
+			"l"."id",
+			"l"."short",
+			"l"."long",
+			"l"."created_at",
+			"l"."updated_at",
+			"l"."deleted_at"
+		FROM "link" "l"
+		%s
+		%s
+		%s
+		%s`,
+		q.BuildDistinct(),
+		q.BuildJoin(),
+		q.BuildWhere(),
+		q.BuildOrder(),
+		q.BuildLimit(),
+	)
+
+	rows, err := r.db.QueryContext(ctx, query, q.Arguments...)
+	if err != nil {
+		r.log.WithError(err).Error("Unable to select Link")
+		return nil, err
+	}
+	defer rows.Close()
+
+	links := []model.Link{}
+	for rows.Next() {
+		link := model.Link{}
+		err := rows.Scan(
+			&link.ID,
+			&link.Short,
+			&link.Long,
+			&link.CreatedAt,
+			&link.UpdatedAt,
+			&link.DeletedAt,
+		)
+		if err != nil {
+			r.log.WithError(err).Error("Unable to scan Link")
+			return nil, err
+		}
+
+		links = append(links, link)
+	}
+
+	if err := rows.Err(); err != nil {
+		r.log.WithError(err).Error("Unable to scan Link")
+		return nil, err
+	}
+
+	return links, nil
+}
+
+func (r *Link) GetOne(ctx context.Context) (*model.Link, error) {
+	links, err := r.Get(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	if len(links) == 0 {
+		return nil, sql.ErrNoRows
+	}
+
+	return &links[0], nil
+}
+
+func (r *Link) GetMap(ctx context.Context) (map[int64]model.Link, error) {
+	list, err := r.Get(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	links := make(map[int64]model.Link, len(list))
+	for _, link := range list {
+		links[link.ID] = link
+	}
+
+	return links, nil
+}
+
+func (r *Link) GetCount(ctx context.Context) (int, error) {
+	q := r.q
+	if !r.withDel {
+		q.Where = append(q.Where, `"l"."deleted_at" IS NULL`)
+	}
+
+	query := fmt.Sprintf(`
+		SELECT COUNT("t".*)
+		FROM (
+			SELECT
+				%s
+				"l"."id"
+			FROM "link" "l"
+			%s
+			%s
+		) "t"`,
+		q.BuildDistinct(),
+		q.BuildJoin(),
+		q.BuildWhere(),
+	)
+
+	row := r.db.QueryRowContext(ctx, query, q.Arguments...)
+
+	var count int
+	if err := row.Scan(&count); err != nil {
+		r.log.WithError(err).Error("Unable to select Link")
+		return 0, err
+	}
+
+	return count, nil
+}
+
+func (r *Link) Distinct(options ...DistinctOption) *Link {
+	q := r.q
+	q.AddDistinct(options...)
+	return &Link{r.log, r.db, q, r.withDel}
+}
+
+func (r *Link) Where(options ...WhereOption) *Link {
+	q := r.q
+	q.AddWhere(options...)
+	return &Link{r.log, r.db, q, r.withDel}
+}
+
+func (r *Link) Order(options ...OrderOption) *Link {
+	q := r.q
+	q.AddOrder(options...)
+	return &Link{r.log, r.db, q, r.withDel}
+}
+
+func (r *Link) Limit(limit int64) *Link {
+	q := r.q
+	q.SetLimit(limit)
+	return &Link{r.log, r.db, q, r.withDel}
+}
+
+func (r *Link) Offset(offset int64) *Link {
+	q := r.q
+	q.SetOffset(offset)
+	return &Link{r.log, r.db, q, r.withDel}
+}
+
+func (r *Link) WithDeleted(include bool) *Link {
+	return &Link{r.log, r.db, r.q, include}
+}
---
+++ internal/repository/repository.go
@@ -0,0 +1,144 @@
+package repository
+
+import (
+	"strconv"
+	"strings"
+)
+
+type QueryOptions struct {
+	Distinct  []string
+	Join      []JoinOption
+	Where     []string
+	Arguments []interface{}
+	Order     []string
+	Limit     *int64
+	Offset    *int64
+}
+
+func (q *QueryOptions) IsEmpty() bool {
+	return len(q.Distinct) == 0 &&
+		len(q.Join) == 0 &&
+		len(q.Where) == 0 &&
+		len(q.Order) == 0 &&
+		q.Limit == nil &&
+		q.Offset == nil
+}
+
+func (q *QueryOptions) AddDistinct(options ...DistinctOption) {
+	if len(options) == 0 {
+		q.Distinct = append(q.Distinct, "")
+	}
+
+	for _, o := range options {
+		q.Distinct = append(q.Distinct, string(o))
+	}
+}
+
+func (q *QueryOptions) AddWhere(options ...WhereOption) {
+	for _, o := range options {
+		start := len(q.Arguments) + 1
+		stmt, args := o.GetWhere(start)
+
+		q.Where = append(q.Where, stmt)
+		q.Arguments = append(q.Arguments, args...)
+
+		if owj, ok := o.(WhereOptionWithJoin); ok {
+			q.Join = append(q.Join, owj.GetJoin()...)
+		}
+	}
+}
+
+func (q *QueryOptions) AddOrder(options ...OrderOption) {
+	for _, o := range options {
+		q.Order = append(q.Order, string(o))
+	}
+}
+
+func (q *QueryOptions) SetLimit(limit int64) {
+	q.Limit = &limit
+}
+
+func (q *QueryOptions) SetOffset(offset int64) {
+	q.Offset = &offset
+}
+
+func (q *QueryOptions) BuildDistinct() string {
+	if len(q.Distinct) == 0 {
+		return ""
+	}
+
+	if q.Distinct[0] == "" {
+		return "DISTINCT"
+	}
+
+	return "DISTINCT ON (" + strings.Join(q.Distinct, ",") + ")"
+}
+
+func (q *QueryOptions) BuildJoin() string {
+	stmt := make([]string, 0, len(q.Join))
+	tables := make(map[string]struct{}, len(q.Join))
+
+	for _, j := range q.Join {
+		if _, ok := tables[j.Table]; ok {
+			continue
+		}
+
+		stmt = append(stmt, j.Build())
+		tables[j.Table] = struct{}{}
+	}
+
+	return strings.Join(stmt, "\n")
+}
+
+func (q *QueryOptions) BuildWhere() string {
+	if len(q.Where) == 0 {
+		return ""
+	}
+
+	return "WHERE (" + strings.Join(q.Where, ") AND (") + ")"
+}
+
+func (q *QueryOptions) BuildOrder() string {
+	if len(q.Order) == 0 {
+		return ""
+	}
+
+	return "ORDER BY " + strings.Join(q.Order, ",")
+}
+
+func (q *QueryOptions) BuildLimit() string {
+	stmt := ""
+
+	if q.Limit != nil {
+		stmt += "LIMIT " + strconv.FormatInt(*q.Limit, 10)
+	}
+
+	if q.Offset != nil {
+		stmt += " OFFSET " + strconv.FormatInt(*q.Offset, 10)
+	}
+
+	return stmt
+}
+
+type JoinOption struct {
+	Type  string
+	Table string
+	On    string
+}
+
+func (j *JoinOption) Build() string {
+	return j.Type + " JOIN " + j.Table + " ON " + j.On
+}
+
+type WhereOption interface {
+	GetWhere(start int) (stmt string, args []interface{})
+}
+
+type WhereOptionWithJoin interface {
+	WhereOption
+	GetJoin() []JoinOption
+}
+
+type DistinctOption string
+
+type OrderOption string
